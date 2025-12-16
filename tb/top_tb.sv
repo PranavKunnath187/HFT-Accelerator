f216e385c10660b1bcc2ae4dc218c00d08dcc932
Pranav Kunnath <pranav.krishna.kunnath@gmail.com>
2025-12-15 17:47:39 -0800

    added some design & tb files to work with

---
+++ tb/top_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/15/2025 05:46:04 PM
+// Design Name: 
+// Module Name: top_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_tb(
+
+    );
+endmodule
